2b276636827b3d6dea850666ca793a12d436a9aa
machtiabdel <machtiabdel@gmail.com>
2020-04-07 23:33:17 +0200

    Checkout Order and Auth process

--- ecommerce/src/ecommerce/forms.py
+++ ecommerce/src/ecommerce/forms.py
@@ -33,38 +33,4 @@ class ContactForm(forms.Form):
             raise forms.ValidationError("Email is not gmail")
         return email
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
 
-User =get_user_model()
-class RegisterForm(forms.Form):
-    firstname = forms.CharField()
-    secondname = forms.CharField()
-    adresse = forms.CharField()
-    email = forms.EmailField()
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    repassword =forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-    def clean_username(self):
-        username = self.cleaned_data.get("username")
-        qs= User.objects.filter(username=username)
-        if qs.exists():
-            raise forms.ValidationError("Username is taken!!")
-        return username
-    
-    def clean_email(self):
-        email=self.cleaned_data.get("email")
-        qs= User.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("Email is taken!!")
-        return email
-        
-    def clean(self):
-        data= self.cleaned_data
-        password= data.get("password")
-        confirmpassword=data.get("repassword")
-        if confirmpassword != password:
-            raise forms.ValidationError("Password must  match!!")
-        return data
--- ecommerce/src/ecommerce/views.py
+++ ecommerce/src/ecommerce/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import ContactForm, LoginForm, RegisterForm
+from .forms import ContactForm
 from django.contrib.auth import authenticate, login, get_user_model
 
 def home_page(request):
@@ -34,46 +34,6 @@ def contact_page(request):
         print(request.POST.get('fullname')) """
         
     return render(request, "contact/view.html",context)
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context={
-        "title":"Login",
-        "form":form
-    }
-    print("User logged in")
-    print(request.user.is_authenticated)
-    if form.is_valid():
-        print(form.cleaned_data)
-        username=form.cleaned_data.get("username")
-        password=form.cleaned_data.get("password")
-        user= authenticate(request, username=username, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            #Redirect to a success page.
-            #context ['form'] = LoginForm()
-            return redirect("/")
-        else:
-            #Return an invalid login erro message
-            print("invalid login")
-    
-    return render(request, "auth/login.html", context)
-
-User = get_user_model()
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context={
-        "title":"Register",
-        "form":form
-    }
-    if form.is_valid():
-        print(form.cleaned_data)
-        username=form.cleaned_data.get("username")
-        password=form.cleaned_data.get("password")
-        email=form.cleaned_data.get("email")
-        new_user= User.objects.create_user(username,email,password)
-        print(new_user)
-    return render(request, "auth/register.html", context)
 
 def home_page_old(request):
     html_="""
